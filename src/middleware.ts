dade93d8a0ee9b433f859385f5f1e2575c734bea
thiago <thiagofelizola@gmail.com>
2025-08-03 19:00:29 -0300

    Update middleware to disable locale detection: Set localeDetection to false for improved localization handling.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import { i18nConfig } from './i18n/config'
 export default createMiddleware({
   locales: i18nConfig.locales,
   defaultLocale: i18nConfig.defaultLocale,
-  localePrefix: 'as-needed'
+  localePrefix: 'as-needed',
+  localeDetection: false
 })
 
 export const config = {
